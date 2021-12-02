96a845922c1d51fef1cafac411d93e7f2f55065c
44lr <73897958+44lr@users.noreply.github.com>
2021-12-02 04:48:18 -0300

    changed spaces / newlines

--- lua/gdr/sv_config.lua
+++ lua/gdr/sv_config.lua
@@ -1,7 +1,7 @@
 tGDRConfig = {
     
     -- address of where the node server is hosted
-    -- remember to run srcds with -allowlocalhttp if you're running the node server locally 
+    -- remember to run srcds with -allowlocalhttp if you're running the node server locally
     Endpoint = "http://127.0.0.1:1337",
 
     -- how many times per second will shall query discord messages
